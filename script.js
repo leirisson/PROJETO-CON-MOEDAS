40d3feb0a9ef03193fa904f22948cb25a8d4ab1e
leirisson <leirissonsouza99@gmail.com>
2025-03-13 15:50:31 -0400

    Criar a função para formatar moeda

--- script.js
+++ script.js
@@ -2,6 +2,16 @@
 const selectElement = document.querySelector('#currency')
 const inputElement = document.querySelector('#amount')
 const form = document.querySelector("form")
+const footer = document.querySelector("main footer")
+const description = document.querySelector("#description")
+const result = document.querySelector("#result")
+
+
+
+// cotação de moedas do dia 
+const USD = 6.15
+const EUR = 5.32
+const GBP = 6.08
 
 
 // Manipulando o input para receber somente numeros
@@ -16,5 +26,48 @@ inputElement.addEventListener("input", () => {
 form.addEventListener("submit", (e) => {
     e.preventDefault()
 
-    console.log(selectElement.value)
+    const amount = inputElement.value
+    const tipoMoeda = selectElement.value
+
+    switch (tipoMoeda) {
+        case 'USD':
+            convertCurrency(amount, USD, 'US$')
+            break
+        case 'EUR':
+            convertCurrency(amount, USD, '€')
+            break
+        case 'GBP':
+            convertCurrency(amount, GBP, '£')
+            break
+    }
+
 })
+
+
+// função para converter a moeda
+function convertCurrency(amount, price, symol) {
+    try {
+        // adciona a classe que exibe o resultado
+        footer.classList.add('show-result')
+
+        description.textContent = `${symol} 1 = ${formatCurrencyBRL(price)}`
+
+        let value = amount * price
+        result.textContent = `${formatCurrencyBRL(value)} Reais`
+        
+    } catch (error) {
+        console.log(error)
+        // remove a classe do footer exibe o footer
+        footer.classList.remove("show-result")
+        alert('Não foi possivel converter. Tente novamente mais tarde.')
+    }
+
+}
+
+// formata a moeda em real brasileiro
+function formatCurrencyBRL(value){
+    return Number(value).toLocaleString("pt-br", {
+        style:"currency",
+        currency: "BRL"
+    })
+}
